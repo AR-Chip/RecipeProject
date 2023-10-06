fc90db747aaf76439bd71381e01479c3fdeddaf9
Arjess Reyes <evilares101@hotmail.com>
2023-10-06 15:22:52 -0500

    Removed validations for removed Ingredients Count

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,6 @@ class Ingredient < ApplicationRecord
   has_many :ingredient_recipe, dependent: :destroy
   has_many :recipe, through: :ingredient_recipe, dependent: :destroy
 
-  validates :name, :count, presence: true
+  validates :name, presence: true
   validates :name, uniqueness: true
-  validates :count, numericality: {only_integers: true}
 end
