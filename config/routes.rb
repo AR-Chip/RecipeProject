8a543430319e90dd163b4e15e1109ccccacfa34f
Arjess Reyes <evilares101@hotmail.com>
2023-10-10 13:29:09 -0500

    Imeplmented About Us Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # Route to Recipes
   resources :recipes, only: [:index, :show]
+
+  # Route to Categories
   resources :categories, only: [:index, :show]
+
+  # Route to About Us
+  get 'about', to: 'static_pages#about'
 
   # Setting Default Page
   root to: 'recipes#index'
