0989d936defe512d6a0374e087db7649c026950f
Joompio <ryan.victoria.731@gmail.com>
2023-10-12 13:27:33 -0500

    change search function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_132649) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_133238) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
