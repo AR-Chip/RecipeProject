a5c447c647bd6681996540bc272c9b0d7bb1099a
Arjess Reyes <evilares101@hotmail.com>
2023-10-11 22:02:15 -0500

    Added Pagination for the Results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,9 @@ class SearchController < ApplicationController
     # Filter by category if :category parameter is present
     if params[:category].present?
       @category = Category.find(params[:category])
-      @recipes = @category.recipes.where('name LIKE ?', "%#{@query}%")
+      @recipes = @category.recipes.where('name LIKE ?', "%#{@query}%").page(params[:page]).per(10)
     else
-      @recipes = Recipe.where('name LIKE ?', "%#{@query}%")
+      @recipes = Recipe.where('name LIKE ?', "%#{@query}%").page(params[:page]).per(10)
     end
   end
 end
