6ecde354411e47d9ed9f9771efe4c9657825942a
Arjess Reyes <evilares101@hotmail.com>
2023-10-11 21:59:36 -0500

    Implemented Hierarchical Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,13 @@ class SearchController < ApplicationController
   def results
     # Getting Access to User Submitted Query
     @query = params[:q]
-    @recipes = Recipe.where('name LIKE ?', "%#{@query}%")
+
+    # Filter by category if :category parameter is present
+    if params[:category].present?
+      @category = Category.find(params[:category])
+      @recipes = @category.recipes.where('name LIKE ?', "%#{@query}%")
+    else
+      @recipes = Recipe.where('name LIKE ?', "%#{@query}%")
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :category_recipe, dependent: :destroy
-  has_many :recipe, through: :category_recipe, dependent: :destroy
+  has_many :recipes, through: :category_recipe, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
